5b19fe320a628848377c4321b45f1d33eeb6ddd3
Ikede Emeoghene Jude <judeikede@gmail.com>
2021-07-30 01:03:06 +0100

    password hashed

--- routes/userroutes.js
+++ routes/userroutes.js
@@ -1,19 +1,27 @@
 const express = require('express');
 const router = express.Router();
-
+const bcrypt = require(bcryptjs)
 const User = require('../models/User');
+const {signupValidation, loginValidaion} = require('../validation/validations');
 // console.log(joi)
 
 router.post('/signup', async (req,res) => {
 //input validation
-   
+   const {error} = signupValidation(req.body)
    if(error) return res.status(400).send(error.details[0].message);
+
+//checking for existing email
+const emailExist = await User.findOne({email: req.body.email});
+if(emailExist) return res.status(400).send('Email already Exist')   
+
+//hash  the password
+const hashedPassword = await bcrypt.hash(req.body.password, 12)
 try {
   let user = new User({
         firstName: req.body.firstName,
         lastName:  req.body.lastName,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
 
     });
 
@@ -25,7 +33,21 @@ try {
     console.log(error);
 }
 
-router.post('/products')
+router.post('/login', async (req, res) =>{
+
+    
+   const {error} = loginValidation(req.body)
+    if(error) return res.status(400).send(error.details[0].message);
+
+   //checking for existing email
+const emailExist = await User.findOne({email: req.body.email});
+if(!emailExist) return res.status(400).send('Email not found');
+
+//check password
+const checkPassword = await bcrypt.compare(req.body.password, user.password);
+if(!checkPassword) res.status(401).send('incorrect password');
+
+})
 
 });
 
